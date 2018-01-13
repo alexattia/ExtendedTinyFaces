faa9543037f543d2b3b48bc9a4106e408f2c9f31
Alexandre Attia <alexattia18@gmail.com>
2018-01-13 14:30:48 +0100

    use TF non max suppresion instead of custom one

--- evaluate.py
+++ evaluate.py
@@ -227,28 +227,3 @@ def evaluate(weight_file_path,  output_dir=None, data_dir=None, img=None, list_i
     final_bboxes = final_bboxes[0]
   return final_bboxes
 
-def main():
-
-  argparse = ArgumentParser()
-  argparse.add_argument('--weight_file_path', type=str, help='Pretrained weight file.', default="/path/to/mat2tf.pkl")
-  argparse.add_argument('--data_dir', type=str, help='Image data directory.', default="/path/to/input_image_directory")
-  argparse.add_argument('--output_dir', type=str, help='Output directory for images with faces detected.', default="/path/to/output_directory")
-  argparse.add_argument('--prob_thresh', type=float, help='The threshold of detection confidence(default: 0.5).', default=0.5)
-  argparse.add_argument('--nms_thresh', type=float, help='The overlap threshold of non maximum suppression(default: 0.1).', default=0.1)
-  argparse.add_argument('--line_width', type=int, help='Line width of bounding boxes(0: auto).', default=3)
-  argparse.add_argument('--display', type=bool, help='Display each image on window.', default=False)
-
-  args = argparse.parse_args()
-
-  # check arguments
-  assert os.path.exists(args.weight_file_path), "weight file: " + args.weight_file_path + " not found."
-  assert os.path.exists(args.data_dir), "data directory: " + args.data_dir + " not found."
-  assert os.path.exists(args.output_dir), "output directory: " + args.output_dir + " not found."
-  assert args.line_width >= 0, "line_width should be >= 0."
-
-  with tf.Graph().as_default():
-    evaluate(
-      weight_file_path=args.weight_file_path, data_dir=args.data_dir, output_dir=args.output_dir,
-      prob_thresh=args.prob_thresh, nms_thresh=args.nms_thresh,
-      lw=args.line_width, display=args.display)
-
